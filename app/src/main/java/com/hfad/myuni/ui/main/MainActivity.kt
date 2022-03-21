fdfbd03ffd8f8fa7c5efce393fd6777b374e7c50
DzikiOwoc15 <lukasiwa@poczta.onet.pl>
2022-03-21 22:57:58 +0100

    Add task minor fix and gradle build changes

--- app/src/main/java/com/hfad/myuni/ui/main/MainActivity.kt
+++ app/src/main/java/com/hfad/myuni/ui/main/MainActivity.kt
@@ -1,33 +1,4 @@
-<<<<<<< HEAD:app/src/main/java/com/hfad/myuni/ui/main/MainActivity.kt
 package com.hfad.myuni.ui.main
-
-import android.content.Intent
-import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import com.hfad.myuni.AddTaskActivity
-import com.hfad.myuni.R
-
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-
-        fab.setOnClickListener { view ->
-            startActivity(Intent(this, AddTaskActivity::class.java))
-        }
-    }
-=======
-package com.hfad.myuni
 
 import android.content.Intent
 import android.os.Bundle
@@ -39,10 +10,9 @@ import androidx.viewpager.widget.ViewPager
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
+import com.hfad.myuni.R
 import com.hfad.myuni.ui.addTask.AddTaskActivity
 import com.hfad.myuni.ui.backEnd.BackEndViewModel
-import com.hfad.myuni.ui.main.ConnectionViewModel
-import com.hfad.myuni.ui.main.SectionsPagerAdapter
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
@@ -99,5 +69,4 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
->>>>>>> e082834ab7b28ae059bd643753223e2bcc5c134b:app/src/main/java/com/hfad/myuni/MainActivity.kt
 }
